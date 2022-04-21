8b97ff048c9029923ac932091d6cbeee989c05a8
tjsrbKR <kkil5205@gmail.com>
2022-04-21 11:05:54 +0900

    회원가입 기능구현 완료

--- js/join.js
+++ js/join.js
@@ -1,11 +1,75 @@
-const $id = document.getElementById("ID");
-const $pw = document.getElementById("PW");
+const $Id = document.getElementById("ID");
+const $Pw = document.getElementById("PW");
 const $login = document.getElementById("BtnLogin");
 const $BtnJoin = document.getElementById("BtnJoin");
+const $IdCkText = document.getElementById("IdCkText");
+const $PWmsg = document.getElementById("PWmsg");
+const $PWCK = document.getElementById("PWCK"); 
+$Id.addEventListener("focusout",IDCk);
+$PWCK.addEventListener("focusout",PWLength);
+$BtnJoin.addEventListener("click",Submit);
+let temp = 0;
+function IDCk(){
+        for(let i=0;i<register.length; i++){
+            if($Id.value === register[i].id){
+                $IdCkText.textContent = ''; 
+                $IdCkText.textContent += "중복된 아이디 입니다.";
+                $IdCkText.style.color = "red";
+                // alert("중복된 아이디 입니다.");
+                return false;
+            }
+            else if($Id.value.length < 4){
+                $IdCkText.textContent = ''; 
+                $IdCkText.textContent += "아이디를 4자 이상 입력해 주세요";
+                $IdCkText.style.color = "red";
+                
+                // alert("아이디를 5자 이상 입력해 주세요");
+                return false;
+            }
+            else{
+                $IdCkText.textContent = ''; 
+                $IdCkText.textContent += "사용 가능한 아이디 입니다.";
+                $IdCkText.style.color = "green";
+                temp++;
+                // alert("사용 가능한 아이디 입니다.");
+                return false;
+            }
+        }
+    
+}
+function PWLength(){
+    if($Pw.value != $PWCK.value){
+        $PWmsg.textContent = '';
+        $PWmsg.textContent += "비밀번호가 일치하지 않습니다.";
+        $PWmsg.style.color = "red";
+        
+    }
+    else if ($PWCK.value.length < 5){
+        $PWmsg.textContent = '';
+        $PWmsg.textContent += "비밀번호를 6자리 이상 입력해 주세요";
+        $PWmsg.style.color = "red";
 
+    }
+    else {
+        $PWmsg.textContent = '';
+        $PWmsg.textContent += "비밀번호가 일치합니다.";
+        $PWmsg.style.color = "green";
+        temp++;
+    }
+}
+
+function Submit(){
+    if(temp === 2){
+        location.href="login.html";
+    }
+    else{
+        alert("회원가입 성공");
+        return false;
+    }
+}
 function Enter(e){
     if(e.keyCode === 13){
         discrimination();
-        $id.focus();
+        $Id.focus();
     }
 }
